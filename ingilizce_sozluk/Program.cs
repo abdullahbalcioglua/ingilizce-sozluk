ed3ecdbdfa387fcc35b4cd7796750dc32664ddf9
abdullah balcıoglu <abdullahbalcoglu@gmail.com>
2019-04-28 17:13:18 +0300

    ilk versiyona geri dönüldü

--- ingilizce_sozluk/Program.cs
+++ ingilizce_sozluk/Program.cs
@@ -16,7 +16,7 @@ namespace ingilizce_sozluk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
         }
     }
 }
